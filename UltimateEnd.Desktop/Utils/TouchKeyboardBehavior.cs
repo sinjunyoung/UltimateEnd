b0272f44de911af08ff2ba968d833d3765ad53c4
sinjunyoung <sinjunyoung1@gmail.com>
2026-01-18 08:54:25 +0900

    터치 키보드 버그 수정

--- UltimateEnd.Desktop/Utils/TouchKeyboardBehavior.cs
+++ UltimateEnd.Desktop/Utils/TouchKeyboardBehavior.cs
@@ -10,17 +10,15 @@ namespace UltimateEnd.Desktop.Utils
 {
     public partial class TouchKeyboardBehavior
     {
-        [Guid("37c994e7-432b-4834-a2f7-dce1f13b834b")]
-        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        interface ITipInvocation
-        {
-            void Toggle(IntPtr hwnd);
-        }
 
         [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
         private static extern IntPtr FindWindow(string lpClassName, string? lpWindowName);
 
-        public static readonly AttachedProperty<bool> EnableProperty = AvaloniaProperty.RegisterAttached<TouchKeyboardBehavior, Control, bool>("Enable", defaultValue: false);
+        [DllImport("user32.dll")]
+        private static extern int PostMessage(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
+
+        public static readonly AttachedProperty<bool> EnableProperty =
+            AvaloniaProperty.RegisterAttached<TouchKeyboardBehavior, Control, bool>("Enable", defaultValue: false);
 
         static TouchKeyboardBehavior() => EnableProperty.Changed.Subscribe(OnEnableChanged);
 
@@ -33,53 +31,28 @@ namespace UltimateEnd.Desktop.Utils
                         ShowKeyboard();
                 }, RoutingStrategies.Tunnel, true);
 
-                textBox.LostFocus += (s, e) => HideKeyboard();                
+                textBox.LostFocus += (s, e) => HideKeyboard();
             }
         }
 
         public static void ShowKeyboard()
         {
-            try
-            {
-                Guid CLSID_UIHostNoLaunch = new("4ce576fa-83dc-4f88-951c-9d0782b4e376");
-                Type? comType = Type.GetTypeFromCLSID(CLSID_UIHostNoLaunch);
-
-                if (comType != null)
-                {
-                    object? instance = Activator.CreateInstance(comType);
-
-                    if (instance is ITipInvocation tip)
-                    {
-                        tip.Toggle(IntPtr.Zero);
-                        return;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"COM 호출 에러: {ex.Message}");
-            }
+            IntPtr hwnd = FindWindow("IPTip_Main_Window", null);
+            if (hwnd != IntPtr.Zero) return;
 
             try
             {
-                Process.Start(new ProcessStartInfo(@"C:\Common Files\Microsoft Shared\ink\TabTip.exe")
-                {
-                    UseShellExecute = true
-                });
+                Process.Start(new ProcessStartInfo("TabTip.exe") { UseShellExecute = true });
+                
             }
             catch { }
         }
 
         public static void HideKeyboard()
         {
-            try
-            {
-                IntPtr hwnd = FindWindow("IPTip_Main_Window", null);
+            IntPtr hwnd = FindWindow("IPTip_Main_Window", null);
 
-                if (hwnd != IntPtr.Zero)
-                    ShowKeyboard();
-            }
-            catch { }
+            if (hwnd != IntPtr.Zero) PostMessage(hwnd, 0x0112, (IntPtr)0xF060, IntPtr.Zero);
         }
     }
 }
